5e7b732168c93f35a546464b97df4b012073f2d5
Gerard <gerard.sans@gmail.com>
2014-03-22 19:30:02 +0000

    initial

---
+++ DesignPatterns/DesignPatterns/Structural/Composite/Component.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Structural.Composite
+{
+    public abstract class Component
+    {
+        public string Name { get; protected set; }
+   
+        public Component(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Structural/Composite/Composite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Structural.Composite
+{
+    public class Composite : Component
+    {
+        protected List<Component> _components = new List<Component>();
+
+        public Composite(string name) : base(name) { }
+
+        public override void Add(Component component)
+        {
+            _components.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            _components.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {            
+            Console.WriteLine(new String('-', depth) + Name);
+
+            // Recursively display child nodes
+            foreach (Component component in _components)
+            {
+                component.Display(depth+2);
+            }
+        }
+    }
+}
