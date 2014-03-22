2992c08814f0b745eb1454669bb6ad158659f442
Gerard <gerard.sans@gmail.com>
2014-03-22 22:30:35 +0000

    Changes

--- DesignPatterns/DesignPatterns/Structural/Proxy/RealSubject.cs
+++ DesignPatterns/DesignPatterns/Structural/Proxy/RealSubject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DesignPatterns
+namespace DesignPatterns.Structural.Proxy
 {
     public class RealSubject : Subject
     {
