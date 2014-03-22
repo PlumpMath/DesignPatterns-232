7f7be49122f60833dfe05e11b2ad0ac6d6de0fa4
Gerard <gerard.sans@gmail.com>
2014-03-22 14:51:52 +0000

    Added note

---
+++ DesignPatterns/DesignPatterns/Creational/Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static object syncLock = new object();
+
+        protected Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (_instance == null)
+                            _instance = new Singleton();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
