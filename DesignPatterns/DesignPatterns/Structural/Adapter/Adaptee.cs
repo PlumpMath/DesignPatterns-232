b68948c70e5914d8eb08936999a37100e6391317
Gerard <gerard.sans@gmail.com>
2014-03-22 17:34:27 +0000

    Initial version

---
+++ DesignPatterns/DesignPatterns/Structural/Adapter/Adaptee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Structural/Adapter/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DesignPatterns.Common;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    public class Client : IClient
+    {
+        public void Run()
+        {            
+            Target target = new Adapter();
+
+            target.Request();
+
+        }
+    }
+}
