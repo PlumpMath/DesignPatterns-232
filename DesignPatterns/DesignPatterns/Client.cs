d19124e2c6f2b684a87b66646c0a9874b38b55c0
Gerard <gerard.sans@gmail.com>
2014-03-22 20:55:35 +0000

    Proxy changes

---
+++ DesignPatterns/DesignPatterns/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DesignPatterns.Common;
+
+namespace DesignPatterns
+{
+    public class Client : IClient
+    {
+        public void Run()
+        {
+            Proxy proxy = new Proxy();
+
+            proxy.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
