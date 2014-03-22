43fdfd3f213a955a930078f9ab2f98a2cf80db23
Gerard <gerard.sans@gmail.com>
2014-03-22 20:14:36 +0000

    Facade changes

---
+++ DesignPatterns/DesignPatterns/Structural/Facade/Facade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Structural.Facade
+{
+    public class Facade
+    {
+        private SubSystemOne _one;
+        private SubSystemTwo _two;
+        private SubSystemThree _three;
+        private SubSystemFour _four;
+
+        public Facade()
+        {
+            _one = new SubSystemOne();
+            _two = new SubSystemTwo();
+            _three = new SubSystemThree();
+            _four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            _one.Method1();
+            _two.Method2();
+            _four.Method4();    
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            _two.Method2();
+            _three.Method3();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Structural/Facade/SubSystemOne.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Structural.Facade
+{
+    public class SubSystemOne
+    {
+        public void Method1()
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+}
